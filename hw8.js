721f246cd5786e45af64b6b034f40bfbcad0ba2f
Dana <danderson36@gmail.com>
2013-12-15 19:16:27 -0500

    HW8 Submission

---
+++ hw8.js
@@ -0,0 +1,34 @@
+/* Concepts used:
+	Using images
+	Basic animations
+	Transformations
+*/
+
+
+window.addEventListener('load', function () {
+  var
+    img = new Image,
+    ctx = document.getElementById('myCanvas').getContext('2d');
+
+    ctx.font="30px Arial";
+  
+  img.src = 'js.jpg';
+  img.addEventListener('load', function () {
+
+    var interval = setInterval(function() {
+      var x = 0, y = 0;
+      
+      return function () {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.drawImage(img, x, y);        
+        ctx.strokeText("JavaScript", 300, 250);
+        x += 1;
+        if (x > ctx.canvas.width) {
+          x = 0;
+        }
+      };
+    }(), 1000/40);
+  }, false);
+}, false);
+
+
