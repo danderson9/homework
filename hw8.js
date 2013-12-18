61fe1ae9206b2b774bf9c9562d1b8607e03fc242
Jose Galdamez <mcstudent@jose-galdamezs-computer.local>
2013-12-17 21:26:57 -0500

    Homework 8 fixes

--- hw8.js
+++ hw8.js
@@ -5,30 +5,28 @@
 */
 
 
-window.addEventListener('load', function () {
-  var
-    img = new Image,
-    ctx = document.getElementById('myCanvas').getContext('2d');
+window.addEventListener('load', function() {
+	var img = new Image,
+		ctx = document.getElementById('myCanvas').getContext('2d');
 
-    ctx.font="30px Arial";
-  
-  img.src = 'js.jpg';
-  img.addEventListener('load', function () {
+	ctx.font = "30px Arial";
 
-    var interval = setInterval(function() {
-      var x = 0, y = 0;
-      
-      return function () {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        ctx.drawImage(img, x, y);        
-        ctx.strokeText("JavaScript", 300, 250);
-        x += 1;
-        if (x > ctx.canvas.width) {
-          x = 0;
-        }
-      };
-    }(), 1000/40);
-  }, false);
+	img.src = 'js.png';
+	img.addEventListener('load', function() {
+
+		var interval = setInterval(function() {
+			var x = 0,
+				y = 0;
+
+			return function() {
+				ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+				ctx.drawImage(img, x, y);
+				ctx.strokeText("JavaScript", 300, 250);
+				x += 1;
+				if (x > ctx.canvas.width) {
+					x = 0;
+				}
+			};
+		}(), 1000 / 40);
+	}, false);
 }, false);
-
-
